1970b6f41052e2a6ed2f88d5df1069a5a461c084
bognar marton <bognarmarci@gmail.com>
2014-08-24 23:09:43 +0200

    Add database schema

---
+++ schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `subjects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `level` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
