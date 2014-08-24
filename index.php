f57e2670338ef72442c6c79ccde91e4915d5a430
bognar marton <bognarmarci@gmail.com>
2014-08-24 22:58:14 +0200

    Basic stuff

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
-ob_start("ob_gzhandler");
+ob_start('ob_gzhandler');
 
 session_start();
 
+require_once 'config.php';
 require_once 'vendor/autoload.php';
 
 Twig_Autoloader::register();
@@ -18,4 +19,27 @@ $engine = new MarkdownEngine\MichelfMarkdownEngine();
 
 $twig->addExtension(new MarkdownExtension($engine));
 
-echo $twig->render('index.html');
+try {
+
+    $con = new PDO('mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['dbname'] . ';charset=utf8', $config['db']['username'], $config['db']['password']);
+    $con->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $con->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+
+    die('Could not connect to the database.');
+
+}
+
+$getSubjects = $con->query('select * from subjects');
+
+$subjects = array();
+
+while ($subject = $getSubjects->fetch()) {
+
+    $subjects[] = $subject;
+
+}
+
+echo $twig->render('index.html', array('subjects' => $subjects));
