894622dab3650b35ab4366f88056b767c1ac1dd3
bognar marton <bognarmarci@gmail.com>
2014-08-24 22:23:04 +0200

    Start using twig

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-erettsegik.hu
+<?php
+
+ob_start("ob_gzhandler");
+
+session_start();
+
+require_once 'vendor/autoload.php';
+
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+
+use Aptoma\Twig\Extension\MarkdownExtension;
+use Aptoma\Twig\Extension\MarkdownEngine;
+
+$engine = new MarkdownEngine\MichelfMarkdownEngine();
+
+$twig->addExtension(new MarkdownExtension($engine));
+
+echo $twig->render('index.html');
