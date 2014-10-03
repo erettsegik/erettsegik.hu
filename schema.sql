e3acc8a82e7d2217a016069e04ddfa500f241768
marton bognar <bognarmarci@gmail.com>
2014-10-03 20:40:49 +0200

    Add modifications table to the database

--- schema.sql
+++ schema.sql
@@ -1,3 +1,17 @@
+CREATE TABLE IF NOT EXISTS `modifications` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `noteid` int(11) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `original_text` text NOT NULL,
+  `new_text` text NOT NULL,
+  `comment` text NOT NULL,
+  `date` date NOT NULL,
+  `updatedate` date NOT NULL,
+  `status` int(11) NOT NULL,
+  `reply` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
 CREATE TABLE IF NOT EXISTS `notes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
