91c3b9912fc51fa07ed5b9af4734ba2e7591393b
marton bognar <martonbognar@sch.bme.hu>
2015-05-30 19:29:34 +0200

    Fix about title

--- inc/about.php
+++ inc/about.php
@@ -2,6 +2,8 @@
 
 $index_var['location'][] = array('url' => '/about/', 'name' => 'Rólunk');
 
+$index_var['title'] = 'Rólunk';
+
 $rendertarget = $_SESSION['mobile'] ? 'mobile/about.html' : 'about.html';
 
 echo $twig->render($rendertarget, array('index_var' => $index_var));
