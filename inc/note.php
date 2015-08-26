5d33af1c787874d17f74f7d16b8076807608a598
marton bognar <martonbognar@sch.bme.hu>
2015-08-26 16:29:01 +0200

    Fix timezones and some styling

--- inc/note.php
+++ inc/note.php
@@ -177,7 +177,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'add') {
 
   }
 
-  if (isset($_POST['learned-submit']) && isset($_SESSION['userid']) && isValid('note', $_GET['id'])) {
+  if (isset($_POST['learned']) && isset($_SESSION['userid']) && isValid('note', $_GET['id'])) {
 
     $learned = isset($_POST['learned']) && $_POST['learned'] == 'on';
 
